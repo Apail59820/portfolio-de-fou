fd549d87edfce89a9885b049aa196342ffea4192
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2026-01-26 08:33:02 +0100

    Profile card unmocked

--- components/ContactSection/ContactSection.tsx
+++ components/ContactSection/ContactSection.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import styles from './ContactSection.module.css'
 import SectionTitle from "@/app/_components/SectionTitle";
+import ProfileCard from "@/components/ProfileCard/ProfileCard";
 
 export default function ContactSection() {
 
@@ -8,6 +11,22 @@ export default function ContactSection() {
       <SectionTitle>
         Me contacter
       </SectionTitle>
+      <ProfileCard
+        name="Amaury Paillart"
+        title="Développeur Web"
+        handle="amauryp-paillart"
+        status="Online"
+        contactText="Contact Me"
+        avatarUrl="/amaury.png"
+        grainUrl={'/profilecard/grain.webp'}
+        iconUrl={'/profilecard/iconpattern.png'}
+        showUserInfo
+        enableTilt={true}
+        enableMobileTilt={false}
+        onContactClick={() => console.log('Contact clicked')}
+        behindGlowColor="rgba(125, 190, 255, 0.67)"
+      />
+
     </section>
   )
 }
